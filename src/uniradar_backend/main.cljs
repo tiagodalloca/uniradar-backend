004f47cc3ccab72f671f18a17031cffd930508ad
Tiago Dall'Oca <tdalloca@gmail.com>
2022-11-02 18:13:16 -0300

    code ready for hello world

---
+++ src/uniradar_backend/main.cljs
@@ -0,0 +1,24 @@
+(ns uniradar-backend.main
+  (:require ["express" :as express]))
+
+(def port 3000)
+
+(defonce app*
+  (atom nil))
+
+(defonce server*
+  (atom nil))
+
+(defn init
+  [& args]
+  (reset! app* (express))
+  (doto ^js @app*
+    (.get "/" (fn [req res]
+                (.send res "Hello World!")))
+    (-> (.listen port (fn []
+                        (println "Started listening on port " port)))
+        (some->> (reset! server*)))))
+
+(comment
+  (init)
+  @server*)
